94a38b416f867de5632dea6fe119171522f4d6b8
Dakota Kallas <kallas.dakota@gmail.com>
2023-03-02 20:58:54 -0600

    Added services

---
+++ client/src/app/services/game.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Game } from '../models/game';
+import { Constants } from '../constants/constants';
+import { Metadata } from '../models/metadata';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GameService {
+  private URL: string = Constants.API_VERSION + '/';
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<Game[]> {
+    return this.http.get<Game[]>(this.URL);
+  }
+
+  getOne(gameId: string): Observable<Game> {
+    return this.http.get<Game>(this.URL + '/gids/' + gameId);
+  }
+
+  getMeta(): Observable<Metadata> {
+    return this.http.get<Metadata>(this.URL + '/meta/');
+  }
+
+  makeMove(game: Game, move: number): Observable<Game> {
+    return this.http.post<Game>(`${this.URL}/${game.id}?move=${move}`, game);
+  }
+
+  create(game: Game, color: number): Observable<Game> {
+    return this.http.post<Game>(`${this.URL}/?color=${color}`, game);
+  }
+}
---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Constants } from '../constants/constants';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { User } from '../models/user';
+import { Metadata } from '../models/metadata';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  URL: string = Constants.API_VERSION + '/users';
+
+  constructor(private http: HttpClient) {}
+
+  update(defaults: Metadata): Observable<User> {
+    return this.http.put<User>(this.URL + '/defaults/', defaults);
+  }
+}
