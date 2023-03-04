df580143a50d4a0b42a40949bb6f305c203d6c9f
Dakota Kallas <kallas.dakota@gmail.com>
2023-03-03 18:37:29 -0600

    Added game component view

--- client/src/app/components/game/game.component.ts
+++ client/src/app/components/game/game.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { GameService } from '../../services/game.service';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
+  game: Game | undefined;
+  private gameId: string = '';
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private gameApi: GameService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.gameId = params['gid'];
+      this.gameApi.getOne(this.gameId).subscribe((game) => {
+        this.game = game;
+      });
+    });
+  }
 }
